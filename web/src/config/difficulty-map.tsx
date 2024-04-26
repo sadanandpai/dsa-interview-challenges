2a3ebe94a494b5c64dc9f5c86639f7e2cc14fd46
Sadanand Pai <sadypai@gmail.com>
2024-04-26 12:18:41 +0530

    Home page setup

--- web/src/config/difficulty-map.tsx
+++ web/src/config/difficulty-map.tsx
@@ -2,8 +2,9 @@ import C1480 from "@/challenges/1480.mdx";
 import C1470 from "@/challenges/1470.mdx";
 import C509 from "@/challenges/509.mdx";
 import C35 from "@/challenges/35.mdx";
+import C118 from "@/challenges/118.mdx";
 
-export const veryEasy = [C1480, C1470, C509, C35];
+export const veryEasy = [C1480, C1470, C509, C35, C118];
 export const easy = [];
 export const medium = [];
 export const hard = [];
