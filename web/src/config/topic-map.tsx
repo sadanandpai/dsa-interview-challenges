4a87255a6382a313740d269093ad8ef68ebd394b
Sadanand Pai <sadypai@gmail.com>
2024-04-26 11:55:00 +0530

    Pascal triangle

--- web/src/config/topic-map.tsx
+++ web/src/config/topic-map.tsx
@@ -2,7 +2,8 @@ import C1480 from "@/challenges/1480.mdx";
 import C1470 from "@/challenges/1470.mdx";
 import C509 from "@/challenges/509.mdx";
 import C35 from "@/challenges/35.mdx";
+import C118 from "@/challenges/118.mdx";
 
-export const array = [C1480, C1470, C509];
+export const array = [C1480, C1470, C509, C118];
 export const string = [];
 export const binarySearch = [C35];
