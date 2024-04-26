f2226880f514ccc0d4d85ca55621d97e2d054f89
Sadanand Pai <sadypai@gmail.com>
2024-04-26 14:03:25 +0530

    link fix

--- web/theme.config.jsx
+++ web/theme.config.jsx
@@ -3,37 +3,37 @@ const metaTags = (
     <link rel="icon" type="image/x-icon" href="/favicon.ico" />
     <meta
       name="description"
-      content="A curated list of data structures and algorithms problems along with the solution in JavaScript to crack engineering interviews"
+      content="A curated list of data structures and algorithms problems along with the solution to crack engineering interviews"
     />
     <meta
       property="og:url"
-      content="hhttps://dsa-interview-challenges.vercel.app/"
+      content="https://dsa-interview-challenges.vercel.app/"
     />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="JavaScript Code Challenges" />
     <meta
       property="og:description"
-      content="A curated list of data structures and algorithms problems along with the solution in JavaScript to crack engineering interviews"
+      content="A curated list of data structures and algorithms problems along with the solution to crack engineering interviews"
     />
     <meta
       name="image"
       property="og:image"
-      content="hhttps://dsa-interview-challenges.vercel.app/programmer.svg"
+      content="https://dsa-interview-challenges.vercel.app/programmer.svg"
     />
     <meta name="twitter:card" content="summary_large_image" />
     <meta property="twitter:domain" content="jscodechallenges.vercel.app" />
     <meta
       property="twitter:url"
-      content="hhttps://dsa-interview-challenges.vercel.app/"
+      content="https://dsa-interview-challenges.vercel.app/"
     />
     <meta name="twitter:title" content="JavaScript Code Challenges" />
     <meta
       name="twitter:description"
-      content="A curated list of data structures and algorithms problems along with the solution in JavaScript to crack engineering interviews"
+      content="A curated list of data structures and algorithms problems along with the solution to crack engineering interviews"
     />
     <meta
       name="twitter:image"
-      content="hhttps://dsa-interview-challenges.vercel.app/programmer.svg"
+      content="https://dsa-interview-challenges.vercel.app/programmer.svg"
     />
     <meta name="author" content="Sadanand Pai" />
   </>
@@ -42,12 +42,29 @@ const metaTags = (
 export default {
   head: metaTags,
   logo: <span>DSA Interview Challenges</span>,
+  docsRepositoryBase:
+    "https://github.com/sadanandpai/dsa-interview-challenges/tree/main/web",
+  search: {
+    placeholder: "Search",
+  },
   project: {
     link: "https://github.com/sadanandpai/dsa-interview-challenges",
   },
   feedback: { content: null },
   toc: { component: null },
   gitTimestamp: null,
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath === "/") {
+      return {
+        titleTemplate: "DSA Interview Challenges",
+      };
+    }
+
+    return {
+      titleTemplate: "%s - DSA Interview Challenges",
+    };
+  },
   footer: {
     text: (
       <span>
