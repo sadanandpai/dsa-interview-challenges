5c494d45148c92188d9a863b71e99db6bbd65720
Sadanand Pai <sadypai@gmail.com>
2024-04-28 11:56:05 +0530

    BS challenge addition

--- web/src/config/difficulty-map.tsx
+++ web/src/config/difficulty-map.tsx
@@ -1,10 +1,12 @@
-import C1480 from "@/challenges/1480.mdx";
-import C1470 from "@/challenges/1470.mdx";
-import C509 from "@/challenges/509.mdx";
+import C34 from "@/challenges/34.mdx";
 import C35 from "@/challenges/35.mdx";
 import C118 from "@/challenges/118.mdx";
+import C509 from "@/challenges/509.mdx";
+import C704 from "@/challenges/704.mdx";
+import C1470 from "@/challenges/1470.mdx";
+import C1480 from "@/challenges/1480.mdx";
 
-export const veryEasy = [C1480, C1470, C509, C35, C118];
-export const easy = [];
+export const veryEasy = [C1480, C1470, C509, C704];
+export const easy = [C35, C34, C118];
 export const medium = [];
 export const hard = [];
--- web/src/config/topic-map.tsx
+++ web/src/config/topic-map.tsx
@@ -1,9 +1,11 @@
-import C1480 from "@/challenges/1480.mdx";
-import C1470 from "@/challenges/1470.mdx";
-import C509 from "@/challenges/509.mdx";
+import C34 from "@/challenges/34.mdx";
 import C35 from "@/challenges/35.mdx";
 import C118 from "@/challenges/118.mdx";
+import C509 from "@/challenges/509.mdx";
+import C704 from "@/challenges/704.mdx";
+import C1470 from "@/challenges/1470.mdx";
+import C1480 from "@/challenges/1480.mdx";
 
-export const array = [C1480, C1470, C509, C118];
+export const array = [C1480, C1470, C509, C118, C34];
 export const string = [];
-export const binarySearch = [C35];
+export const binarySearch = [C704, C34, C35];
