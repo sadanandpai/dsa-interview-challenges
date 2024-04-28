26f0702857a08a12dd814e853ea45ffe4e07dc02
Sadanand Pai <sadypai@gmail.com>
2024-04-28 13:19:09 +0530

    String reverse

--- web/src/config/difficulty-map.tsx
+++ web/src/config/difficulty-map.tsx
@@ -2,13 +2,14 @@ import C34 from "@/challenges/34.mdx";
 import C35 from "@/challenges/35.mdx";
 import C118 from "@/challenges/118.mdx";
 import C167 from "@/challenges/167.mdx";
+import C344 from "@/challenges/344.mdx";
 import C509 from "@/challenges/509.mdx";
 import C704 from "@/challenges/704.mdx";
 import C744 from "@/challenges/744.mdx";
 import C1470 from "@/challenges/1470.mdx";
 import C1480 from "@/challenges/1480.mdx";
 
-export const veryEasy = [C1480, C1470, C509, C704];
+export const veryEasy = [C344, C1480, C1470, C509, C704];
 export const easy = [C35, C744, C34, C118, C167];
 export const medium = [];
 export const hard = [];
