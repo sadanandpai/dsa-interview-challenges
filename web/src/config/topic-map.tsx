43001e9b8cb002d85de37da1e8f40e70edf45d63
Sadanand Pai <sadypai@gmail.com>
2024-04-29 22:22:44 +0530

    33 challenge addition

--- web/src/config/topic-map.tsx
+++ web/src/config/topic-map.tsx
@@ -1,25 +1,24 @@
-import C34 from "@/challenges/34.mdx";
-import C35 from "@/challenges/35.mdx";
-import C118 from "@/challenges/118.mdx";
-import C153 from "@/challenges/153.mdx";
-import C167 from "@/challenges/167.mdx";
-import C344 from "@/challenges/344.mdx";
-import C509 from "@/challenges/509.mdx";
-import C704 from "@/challenges/704.mdx";
-import C744 from "@/challenges/744.mdx";
-import C1470 from "@/challenges/1470.mdx";
-import C1480 from "@/challenges/1480.mdx";
-import C215 from "@/challenges/215.mdx";
-import C946 from "@/challenges/946.mdx";
-import C1572 from "@/challenges/1572.mdx";
-import C1752 from "@/challenges/1752.mdx";
+import C33 from '@/challenges/33.mdx';
+import C34 from '@/challenges/34.mdx';
+import C35 from '@/challenges/35.mdx';
+import C118 from '@/challenges/118.mdx';
+import C153 from '@/challenges/153.mdx';
+import C167 from '@/challenges/167.mdx';
+import C215 from '@/challenges/215.mdx';
+import C344 from '@/challenges/344.mdx';
+import C509 from '@/challenges/509.mdx';
+import C704 from '@/challenges/704.mdx';
+import C744 from '@/challenges/744.mdx';
+import C946 from '@/challenges/946.mdx';
+import C1470 from '@/challenges/1470.mdx';
+import C1480 from '@/challenges/1480.mdx';
+import C1572 from '@/challenges/1572.mdx';
+import C1752 from '@/challenges/1752.mdx';
 
 export const array = [C1480, C1470, C1752, C1572];
 export const string = [];
-export const binarySearch = [C704, C35, C744, C34, C153];
-export const dynamicProgramming = [C509, C118];
+export const binarySearch = [C704, C35, C744, C34, C153, C33];
 export const twoPointers = [C344, C167];
-export const stacks = [C946];
-export const kthLargest = [C215];
-
-
+export const stack = [C946];
+export const heap = [C215];
+export const dynamicProgramming = [C509, C118];
