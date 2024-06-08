09b589840d1a4d08f83486e8d0d8e1881b11d831
Karthick S <karthicksakthi124@gmail.com>
2024-06-08 18:37:51 +0000

    LeetCode:: 237: Delete node in Linked list

--- web/src/config/topic-map.tsx
+++ web/src/config/topic-map.tsx
@@ -53,7 +53,8 @@ import C1903 from '@/challenges/1903.mdx';
 import C796 from '@/challenges/796.mdx';
 import C14 from '@/challenges/14.mdx';
 import C125 from '@/challenges/125.mdx';
-import C409 from "@/challenges/409.mdx";
+import C409 from '@/challenges/409.mdx';
+import C237 from '@/challenges/237.mdx';
 
 export const math = [C9, C7];
 export const array = [C268, C1929, C136, C1431, C2960, C1365, C1480, C1470, C1752, C1572, C2283, C1748, C485, C283, C189];
@@ -62,9 +63,8 @@ export const binarySearch = [C704, C367, C35, C744, C69, C34, C33, C153, C81, C1
 export const twoPointers = [C344, C26, C167];
 export const stack = [C735, C946];
 export const hashTable = [C1487];
-export const linkedList = [];
+export const linkedList = [C237];
 export const greedy = [C861];
 export const heap = [C215];
 export const dynamicProgramming = [C509, C118];
 export const slidingWindow = [C594];
- 
